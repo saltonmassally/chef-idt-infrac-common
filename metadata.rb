52cd41c12557f2f53f46f1bcaa410775e2c624eb
Salton Massally <salton.massally@gmail.com>
2014-05-15 12:27:35 +0000

    added ntp ability

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,10 @@ maintainer_email 'salton.massally@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures Common utilities we use with most of our servers at iDT Labs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'apt'
 depends 'build-essential'
 depends 'python'
+depends 'timezone-ii'
+depends 'ntp'
